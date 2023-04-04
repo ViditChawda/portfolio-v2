2869be0cc9fa996ca830a7e9c345600c27ae92cd
vidit chawda <viditchawda301@gamil.com>
2023-04-04 15:21:29 +0530

    skills image

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import './skills.css'
+import mypic3 from '../../assests/mypic3.png'
 
 const Skills = () => {
   return (
-    <div className='skill-main-container' id="skills">
-      <div></div>
-      <h1>Skills</h1>
-      <div></div>
+    <div className='s-container'>
+      <div className='skill-main-container' id="skills">
+        <div></div>
+        <h1>Skills</h1>
+        <div></div>
+      </div>
+      <div className='skills-content'>
+        <div>
+          <img src={mypic3} alt="" className='mypic3' />
+        </div>
+      </div>
+
     </div>
+
   )
 }
 
